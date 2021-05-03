6c38376db1b108594e74b56d8d7cce8ce35e6d34
umehara2jp <u.natsu.co@gmail.com>
2021-05-03 12:11:43 +0900

    [モーダルウィンドウ追加]

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
 $(function() {
-    $('.Hamburger').click(function() {
-        $(this).toggleClass('active');
- 
-        if ($(this).hasClass('active')) {
-            $('.Menu').addClass('active');
-        } else {
-            $('.Menu').removeClass('active');
-        }
-    });
+  $('.Hamburger').click(function() {
+      $(this).toggleClass('active');
+
+      if ($(this).hasClass('active')) {
+          $('.Menu').addClass('active');
+      } else {
+          $('.Menu').removeClass('active');
+      }
+  });
 });
+
+
+(function () {
+  if ($(".js-modal-video").length) { //クラス名js-modal-videoがあれば以下を実行
+    $(".js-modal-video").modalVideo({
+      channel: "youtube",
+      youtube: {
+        rel: 0, //関連動画の指定
+        autoplay: 0, //自動再生の指定
+      },
+    });
+  }
+})();
+
